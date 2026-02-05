cc297f75184ff7f0c8a03b9b505cad84eda06355
Aline Isabel <aline.silveira.101932@ga.ita.br>
2026-02-05 15:00:02 -0300

    MPC - Ref variavel - Estimador de perturbacao - Com restricoes
    MPC com Oinf parametrizado com respeito a [x rbar dbar], usando observador pra estimar perturbacoes

--- MPC/ControlePreditivo.m
+++ MPC/ControlePreditivo.m
@@ -2,48 +2,45 @@
 % Codigo geral
 % 03/10/2025
 %-----------------------------------
-clc; clear; close all;
+clc; clearvars -except Oinf1; close all;
 
 % Load Parametros da planta
 Parametros
 
-
 %% Determinar O_psi_inf - todas as refs
-
-Gamma = [eye(nx) zeros(nx,nq); -K (K*Nx+Nu); zeros(nx) Nx; zeros(nu,nx) Nu];
-
 
 % Spsi = [x u xbar ubar]
 Spsi = blkdiag(Sxu,Sxu);
 
 bpsi = [bxu ;bxu - exu];
 
-Apsi_f = [Af B*(K*Nx+Nu); zeros(nq,nx) eye(nq)];
+Gamma = [
+    eye(nx)        zeros(nx,nq)        zeros(nx,nd);          % x
+    -K        (K*Nx+Nu)        (K*Mx+Mu);                       % u
+    zeros(nx)        Nx Mx;           % xbar
+    zeros(nu,nx)        Nu                 Mu                  % ubar
+];
 max_iter = 50;
 
-[So,bo,Si,bi] = Oinf_MAS(Apsi_f,Gamma,Spsi,bpsi,max_iter);
+Apsi = [Af B*(K*Nx+Nu) B*(K*Mx+Mu)+E; zeros(nq,nx) eye(nq) zeros(nq,nd); zeros(nd,nx) zeros(nd,nq) eye(nd)];
+
+[So,bo,Si,bi] = Oinf_MAS(Apsi,Gamma,Spsi,bpsi,max_iter);
+
+Ox = So(:,1:nx);
+Or = So(:,nx+1);
+Od = So(:,nx+2:end);
 
 O_psi_inf = Polyhedron('H',[So bo]); % Declaração de Oinf como objeto do MPT Toolbox
 
-figure (22)
-plot(O_psi_inf);xlabel('x1'); ylabel('x2'); zlabel('r'); title('O_chi_inf')
-
-%% Determinar Oinf a partir de O_psi_inf
-
-Ox = So(:,1:nx);
-Or = So(:,nx+1:end);
 Sf = Ox;
 
-rbar = 1*pi/180; % Valor da referência no instante atual
-xbar = Nx*rbar;
-ubar = Nu*rbar;
-
-bf = bo - Or*rbar;
-
-O_inf = Polyhedron('H',[Sf bf]); % Declaração de Oinf como objeto do MPT Toolbox
-
-figure (21)
-plot(O_inf);xlabel('x1'); ylabel('x2'); title('O_inf - rbar = 1 [deg]')
+% Teste rbar = 1*pi/180 e dbar = 0;
+dbar_teste = 0; rbar_teste = 1*pi/180;
+% xbar_teste = Nx*rbar_teste + Mx*dbar_teste;
+% ubar_teste = Nu*rbar_teste + Mu*dbar_teste;
+bf_teste = bo-Or*rbar_teste-Od*dbar_teste;
+O_inf = Polyhedron('H',[Sf bf_teste]); % Declaração de Oinf como objeto do MPT Toolbox
+figure (120); plot(O_inf); xlabel('x1');ylabel('x2'); title('rbar = 1 [deg]; dbar = 0')
 
 %% Dual Mode Predictive Control
 
@@ -57,38 +54,33 @@ options_qudprog = optimoptions('quadprog','Display','off');
 % Inicializacao
 kend = 100;
 x0 = [1;0];
-
-% Loop
 x(:,1) = x0; 
-% chi_pri(:,1) = [x(:,1);0];
-
-
-sys = ss(Adc,Bc,[],[]);
-trec = 0; xrec = x(:,1)'; nt = 10; 
+chi_pri(:,1) = [x(:,1);0];
+rbar = 1*pi/180;
 
 for k = 1:kend
 
   % Matrizes finais do quadprog 
     % J = f*U'+0.5*U'*H*U
     % s.a A * U < B
-    % zpri(:,k) = Hchi*chi_pri(:,k);
-    % z(:,k) = x(:,k);
-    % chi_pos(:,k) = chi_pri(:,k) + M*(z(:,k) - zpri(:,k));
-    % dbar_pos(:,k) = chi_pos(3,k);
-    % xbar(:,k) = Nx*rbar + Mx*dbar_pos(:,k);
-    % ubar(:,k) = Nu*rbar + Mu*dbar_pos(:,k);
 
-    % bf = bo-Or*rbar-Od*dbar_pos(:,k);
+    zpri(:,k) = Hchi*chi_pri(:,k);
+    z(:,k) = x(:,k);
+    chi_pos(:,k) = chi_pri(:,k) + M*(z(:,k) - zpri(:,k));
+    dbar_pos(:,k) = chi_pos(3,k);
+    xbar(:,k) = Nx*rbar + Mx*dbar_pos(:,k);
+    ubar(:,k) = Nu*rbar + Mu*dbar_pos(:,k);
 
+    bf = bo-Or*rbar-Od*dbar_pos(:,k);
 
     bxu_barra = [bv-Vx*x(:,k); repmat(bv,N-1,1)];
     bx_barra = [repmat(bx,N-1,1);bf];
 
     Aqp_xu = Sxu_u_barra + Sxu_x_barra*Bbarra;
-    bqp_xu = bxu_barra- Sxu_x_barra*(Abarra*x(:,k));
+    bqp_xu = bxu_barra- Sxu_x_barra*(Abarra*x(:,k)+En*repmat(dbar_pos(k),N,1));
 
     Aqp_x = Sx_barra*Bbarra;
-    bqp_x = bx_barra- Sx_barra*(Abarra*x(:,k));
+    bqp_x = bx_barra- Sx_barra*(Abarra*x(:,k)+En*repmat(dbar_pos(k),N,1));
 
     Aqp = [Aqp_xu; Aqp_x];
     bqp = [bqp_xu;bqp_x];
@@ -98,39 +90,28 @@ for k = 1:kend
     % Simetrização
     Hqp = (Hqp + Hqp')/2;
 
-    fqp = (Bbarra'*Qbarra*(Abarra*x(:,k) - repmat(xbar,N,1)) - Rbarra*repmat(ubar,N,1));
+    fqp = (Bbarra'*Qbarra*(Abarra*x(:,k)+En*repmat(dbar_pos(k),N,1) - repmat(xbar(:,k),N,1)) - Rbarra*repmat(ubar(:,k),N,1));
 
     dummy = quadprog(Hqp, fqp,Aqp,bqp, [], [], [], [], [], options_qudprog);
 
-    
     umpc(k) = dummy(1);
     utotal(k) = umpc(k) + a*sin(x(1,k));
-    w(k) = sqrt(utotal(k)/c);
+    
+    chi_pri(:,k+1) = Achi*chi_pos(:,k) + Bchi*umpc(:,k);
 
-    % chi_pri(:,k+1) = Achi*chi_pos(:,k) + Bchi*umpc(:,k);
-
-% % simulacao linear
-    % x(:,k+1) = A*x(:,k)+B*umpc(:,k);
-    % t = linspace((k-1)*T,k*T,nt);
-    % U = repmat(umpc(:,k)',nt,1);
-    % [~,tout,xout] = lsim(sys,U,t,x(:,k));
-    % trec = [trec;tout(2:end)];
-    % xrec = [xrec;xout(2:end,:)];
-    % x(:,k+1) = xrec(end,:)';
-    % Simulacao continua
 	xini = x(:,k);
-    [t, xd] = ode45(@(t,x) simulation(x, umpc(k), a,b,c,Ac,Bc,w(k)),[0 Td_smc], xini, options2); 
+    [t, xd] = ode45(@(t,x) simulation(x, umpc(k), a,b,c,Ac,Bc),[0 Td_smc], xini, options2); 
     x(:,k+1) = xd(length(t),:)';
 
 end
 
 % Simulacao - tempo continuo
-function xdot = simulation(x,u,a,b,c,Ac,Bc,w)
-% w = sqrt((a*sin(x(1))+u)/c);
+function xdot = simulation(x,u,a,b,c,Ac,Bc)
+w = sqrt((a*sin(x(1))+u)/c);
 xdot_1 = x(2);
 xdot_2 = -a*sin(x(1))-b*x(2)+c*w^2;
 xdot = [xdot_1;xdot_2];
-% xdot = Ac*x+Bc*u;
+% xdot = 1.05*Ac*x+Bc*u;
 end
 
 % Plots
@@ -186,5 +167,11 @@ plot(umpc,LineWidth=2)
 hold on; grid on; grid minor
 plot(a*sin(x(1,:)),LineWidth=2)
 legend('umpc','asin(theta)')
+xlim([0 100])
+xlabel('k')
+
+figure(10)
+plot(dbar_pos,LineWidth=2)
+legend('dbar');grid on;grid minor
 xlim([0 100])
 xlabel('k')
